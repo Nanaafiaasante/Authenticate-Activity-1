6741e12de4bd9123f63ba43e4e6d9a306f23b80a
Nana Afia <137131957+Bootcampp@users.noreply.github.com>
2025-10-25 22:31:00 +0000

    update activity

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -13,15 +13,15 @@ if (!defined("SERVER")) {
 }
 
 if (!defined("USERNAME")) {
-    define("USERNAME", "root");
+    define("USERNAME", "nana.asante");
 }
 
 if (!defined("PASSWD")) {
-    define("PASSWD", "");
+    define("PASSWD", "Setumte!");
 }
 
 if (!defined("DATABASE")) {
     // Use the database name from the provided SQL dump
-    define("DATABASE", "shoppn");
+    define("DATABASE", "ecommerce_2025A_nana_asante");
 }
 ?>
